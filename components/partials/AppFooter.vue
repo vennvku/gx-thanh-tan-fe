81f1e1687e907ba3e521d8b3051b2317517f4e52
vennvku <quocvanvku@gmail.com>
2024-07-11 15:58:30 +0700

    feat(ui): create ui footer

--- components/partials/AppFooter.vue
+++ components/partials/AppFooter.vue
@@ -1,6 +1,67 @@
 <template>
-  <div style="height: 100px">
-    <h1>This is Footer</h1>
+  <div class="wrapper-footer">
+    <b-container class="footer cont-wrap" fluid>
+      <div class="list-block-footer">
+        <div class="block-footer block-footer-1">
+          <div class="caption">Giáo xứ Thanh Tân</div>
+          <p class="quick-view">
+            Giáo xứ Thanh Tân, thuộc Giáo hạt Hương Quảng Phong, trên địa bàn xã
+            Phong Sơn, huyện Phong Điền, tỉnh Thừa Thiên-Huế, tọa lạc dưới chân
+            dãy núi Trường Sơn. Nhà thờ Thanh Tân nằm bên tỉnh lộ 11B, cách tòa
+            Giám mục Huế 20,6 km theo đường chim hay 34 km theo đường bộ về phía
+            Tây Tây Bắc
+          </p>
+          <ul class="list-info">
+            <li>
+              <span
+                >Địa chỉ: Thanh Tân, Phong Sơn, Phong Điền, Thừa Thiên Huế</span
+              >
+            </li>
+            <li>
+              <span>Điện Thoại: +84-000-0000-000</span>
+            </li>
+            <li>
+              <span>Email: example@gmail.com</span>
+            </li>
+          </ul>
+        </div>
+        <div class="block-footer block-footer-2">
+          <div class="caption">Fanpage Facebook</div>
+        </div>
+        <div class="block-footer block-footer-3">
+          <div class="caption">Liên kết website</div>
+          <ul>
+            <li>
+              <a
+                href="https://www.vatican.va/content/vatican/en.html"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Toà thánh Vantican</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://hdgmvietnam.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Hội đồng Giám mục Việt Nam</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://tonggiaophanhue.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Tổng Giáo Phận Huế</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="copy-right">
+        Copyright © 2024 Giáo xứ Thanh Tân | Powered by Giáo xứ Thanh Tân
+      </div>
+    </b-container>
   </div>
 </template>
 
@@ -9,3 +70,7 @@ export default {
   name: 'AppFooter',
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/scss/partials/footer';
+</style>
